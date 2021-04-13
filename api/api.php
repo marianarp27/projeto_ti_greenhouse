358397e258ba0f2bdd77ab23eecf3f680890fcc4
Mariana <Pereira>
2021-04-13 15:50:24 +0100

    api corrigida (na aula)

--- api/api.php
+++ api/api.php
@@ -1,34 +1,29 @@
 <?php
     //var_dump(file_get_contents('php://input'));
-    //define('PHP_EOL', "\n");
 
     header('Content-Type: text/html; charset=utf-8');
     //echo $_SERVER['REQUEST_METHOD'];
 
+    
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        echo "recebido um POST";
+        echo "recebido um POST \n";
 
-        $post_array = array('valor'=>'42', 'nome'=>'temperatura', 'hora'=>'2021/03/25 00:31');
-        print_r($post_array);
-
-        if (isset($post_array['valor'],$lpost_arrayist['nome'],$post_array['hora'])) {
-
-            $file_nome = dirname(__FILE__).'\files\temperatura\nome.txt';    
-            file_put_contents($file_nome, $post_array['nome']);
+        //(valor=45&nome=temperatura&hora=2021/03/25 00:31);
+        print_r($_POST);       
+        
+        if ( isset($_POST['nome']) && isset($_POST['valor']) && isset($_POST['hora'])) {
+                       
+            file_put_contents("files/".$_POST["nome"]."/nome.txt", $_POST['nome']);
+           
+            file_put_contents("files/".$_POST["nome"]."/valor.txt", $_POST['valor']);
     
-            $file_valor = dirname(__FILE__).'\files\temperatura\valor.txt';    
-            file_put_contents($file_valor, $post_array['valor']);
-    
-            $file_hora = dirname(__FILE__).'\files\temperatura\hora.txt';    
-            file_put_contents($file_hora, $post_array['hora']);
+            file_put_contents("files/".$_POST["nome"]."/hora.txt", $_POST['hora']);
 
-            // array só com a 'hora' e 'valor'
-            $hora_valor = array($post_array['hora'],$post_array['valor']);
-            $separador = implode(";", $hora_valor);
-            
-            // 'dirname' mostra direturia onde está o ficheiro
-            $file_log = dirname(__FILE__).'\files\temperatura\log.txt'; 
-            file_put_contents($file_log, $separador .PHP_EOL, FILE_APPEND);
+            file_put_contents("files/".$_POST["nome"]."/log.txt", $_POST["hora"].";".$_POST["valor"] . PHP_EOL, FILE_APPEND);
+
+        }else{
+            echo "\n Parametros recebidos não válidos!";
         }
 
  
@@ -40,13 +35,5 @@
     }
     
 
-    //('username'=>$_POST['username'])
-    //$list = array('valor'=>'42', 'nome'=>'temperatura', 'hora'=>'2021/03/25 00:31');
-    //print_r($list);
-
-    //$postArray = array(valor='42'&nome='temperatura'&hora='2021/03/25 00:31');
-    //print_r($postArray);
-
-    
     
 ?>
