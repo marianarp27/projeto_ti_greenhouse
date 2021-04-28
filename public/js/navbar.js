22ef23ba6256bd787e9005372322360e98646d37
Mariana <Pereira>
2021-04-28 08:45:44 +0100

    Menu + JS

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,38 +1,19 @@
 $(document).ready(function () {
+    
     $(function () {
         // código para saber o caminho da página em que se está atualmente
         // window.location.pathname  -> "//TI/Projeto_TI/dashboard.php"
         var url = window.location.pathname;
         var filename = url.substring(url.lastIndexOf('/')+1);
-        
-        // saber o parametro "nome" passado no URL
-        var params = new URLSearchParams(window.location.search);
-        var name = params.get("nome");
-
+    
         //condição para colocar a class 'active' quando se está na respetiva página
         switch (filename) {
             //caso Dashboard/Home
             case "dashboard.php":
                 $(".nav-link").removeClass('active');
                 $("#nav_home").addClass('active');
-                break;
-            //caso Historico
-            case "historico.php":
-                switch (name) {                    
-                    case "luminosidade":
-                        $(".nav-link").removeClass('active');
-                        $("#nav_luz").addClass('active');
-                        break;
-                    case "temperatura":
-                        $(".nav-link").removeClass('active');
-                        $("#nav_temp").addClass('active');
-                        break;
-                    case "humidade":
-                        $(".nav-link").removeClass('active');
-                        $("#nav_humidade").addClass('active');
-                        break;
-                }
-            break;          
+                break;    
+            // o case do hístórico está implementado em php na 'sidebar.php'  
         }
 
     });
@@ -42,7 +23,5 @@ $(document).ready(function () {
         $('.sidebar').toggleClass('toggled');
         $('.content-page').toggleClass('toggled-content');
     });
-
-
 
 });
--- sidenav.php
+++ sidenav.php
@@ -1,9 +1,10 @@
 <?php
-
     // leitura das API's
     $path = 'api/files';
     $files = array_diff(scandir($path), array('..', '.')); 
 
+      // vai buscar o nome '*.php' da página 'corrente' -> para uso da class 'active'
+      $url_file=basename($_SERVER['PHP_SELF']); 
 ?>
 
 <div class="sidebar pt-3">
@@ -15,16 +16,18 @@
 
         <a class="nav-link disabled shadow-sm mt-3"><i class="fas fa-list mr-2"></i>Histórico</a>
 
-        <!-- código para listar todo os sensores -->
-        <?php  foreach ($files as $value) { ?>
-        
-            <a class="nav-link shadow-sm" href="historico.php?nome=<?php echo "$value"?>"><?php echo ucfirst($value) ?></a>
-
-        <?php
-            }
-        ?>
+         <!-- Condição para que apenas o ADMIN tenha acesso ao histórico -->
 
 
+         <a class="nav-link disabled shadow-sm mt-3"><i class="fas fa-list mr-2"></i>Histórico</a>
+            <!-- código que lista todos os sensores do diretório da api -->
+            <?php  foreach ($files as $value) { ?>              
+                <a class="nav-link shadow-sm <?php if($url_file == "historico.php"){ if($_GET['nome'] == "$value") { ?> active <?php }} ?>"
+                href="historico.php?nome=<?php echo "$value"?>"><?php echo ucfirst($value) ?></a>
+            <?php } ?>
+       
+
+        <!-- Fim da Condição do histórico -->
 
         <a class="nav-link disabled shadow-sm mt-3"><i class="fas fa-cog mr-2"></i>Outros</a>
 
