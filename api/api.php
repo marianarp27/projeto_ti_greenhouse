0be9156fd8f8f6947743b194595bd82e9961b1a4
Mariana <Pereira>
2021-04-29 11:11:20 +0100

    api

--- api/api.php
+++ api/api.php
@@ -25,12 +25,10 @@
 
         if ( isset($_GET['nome'])) {
                        
-            //$get_valor = file_get_contents("files/".$_GET['nome']."/valor.txt");
-            //$get_hora = file_get_contents("files/".$_GET['nome']."/hora.txt");
-            $get_log = file_get_contents("files/".$_GET['nome']."/log.txt");
-            //echo "Valor: $get_valor\n";
-            //echo "Hora: $get_hora";
-            echo "$get_log";
+            $get_valor = file_get_contents("files/".$_GET['nome']."/valor.txt");
+            $get_hora = file_get_contents("files/".$_GET['nome']."/hora.txt");
+            echo "Valor: $get_valor\n";
+            echo "Hora: $get_hora";
 
         }else{
             echo "\n Faltam parâmetros no GET";
