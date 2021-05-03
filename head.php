9cbcc16bd84b720d320e8fac75874971ddd25831
Maria Nunes <mariajoao.99@hotmail.com>
2021-05-03 10:36:58 +0100

    comentários head

--- head.php
+++ head.php
@@ -1,14 +1,15 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--  refresh automático a cada 5 segundos -->
-<!--<meta http-equiv="refresh" content="5"> -->
+<meta http-equiv="refresh" content="5">
 
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<!-- < ?php echo time(); ?> -> Serve para carregar o css em alguns browseres -->
 <link rel="stylesheet" href="public/css/nav.css?v=<?php echo time(); ?>">
 <link rel="stylesheet" href="public/css/login.css?v=<?php echo time(); ?>">
 <link rel="stylesheet" href="public/css/geral.css?v=<?php echo time(); ?>">
-<link rel="stylesheet" href="public/css/dashboard.css?v=<?php echo time(); ?>"> <!-- force the CSS to reload - problema -> não estava ler o ficheiro -> ver com os stores -->
+<link rel="stylesheet" href="public/css/dashboard.css?v=<?php echo time(); ?>"> 
 <!-- Favicon -->
 <link rel="icon" type="image/png" href="public/img/favicon.png" />
 <!-- Font-Awesome (icons) -->
