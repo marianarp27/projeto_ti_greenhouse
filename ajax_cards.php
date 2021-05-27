33a1e82f733ec3f5100d535b00d000be3f5b3eab
Mariana <Pereira>
2021-05-27 19:49:38 +0100

    card's

--- ajax_cards.php
+++ ajax_cards.php
@@ -25,26 +25,24 @@
     <div class="col col_card" id="div-to-refresh">
       <div class="card border-light">
         <div class="card-body rounded shadow-sm p-3">
-          <div class="media mb-3">
+          <div class="media">
             <img class="mr-3" width="50" src="public/img/icon_sensor_<?php echo $row[0] ?>.png" onerror="this.src='public/img/icon_sensor_default.png'" alt="Icon de <?php echo $row[0] ?>"">
             <div class=" media-body">
             <h4 class="mb-1"> <b> <?php echo ucfirst($row[1]) . $simbolo ?> </b> </h4>
             <h6 class="mb-1 text-muted"> <?php echo ucfirst($row[0]) ?> </h6>
           </div>
         </div>
+        <!-- restrinção para 'user' -->
+        <?php if ($_SESSION['perfil'] == "admin") { ?>
         <!-- actualização com icon + link de historico-->
-        <div class="pt-3 border-top">
+        <div class="mt-3 pt-3 border-top">
           <span>
             <i class="far fa-calendar-alt mr-1 mt-2 text-muted"></i>
-            <?php echo ucfirst($row[2]) ?>
-
-            <?php if ($_SESSION['perfil'] == "admin") { ?>
+            <?php echo ucfirst($row[2]) ?>           
               <a href="historico.php?nome=<?php echo $row[0] ?>"><i class="fas fa-angle-double-right span_icon"></i><span class="span_card">Historico</span></a>
-            <?php } ?>
-
           </span>
-
         </div>
+        <?php } ?>
       </div>
     </div>
 </div>
